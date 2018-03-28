f3a67b959648d004b07a605b859df17b7b88ba19
Cecy Correa <cecy.correa@returnpath.com>
2018-03-28 18:39:32 -0500

    add options page

--- background/background.js
+++ background/background.js
@@ -1,42 +1,27 @@
 console.log("🐱" + "meow")
 
-// key = API KEY, part = snippet (required), q = the query
-const reqUrl = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCK1LvFVfX_G8fPXtWagZSfNChyMi2UcKk&part=snippet&q=kitten&maxResults=25"
-const videoUrl = "https://www.youtube.com/watch?v="
-// use Kitten Academy Live Stream as default
-const defaultUrl = "https://www.youtube.com/watch?v=X2gcLUZ7qnw"
+const baseUrl = "https://www.youtube.com/watch?v="
 
+// Default kitten live streams from YouTube 
+const videoIds = ["X2gcLUZ7qnw","r9xlYrOzu0c","izTaPcZZAXs", "Rb4l1vntCeM"]
+
+// Get a random integer to use as index for defaultUrls array
 const getRandomInt = () => {
-    let num = Math.random()
-    //range 0 - 24
-    return Math.floor(num * 24)
+	let i = [Math.floor(Math.random() * videoIds.length)]
+	return parseInt(i)
 }
 
-const getCatUrl = () => {
-    let req = new XMLHttpRequest()
-    req.open("GET", reqUrl, true)
-    req.send()
-    req.onload = () => {
-        if (req.readyState === 4 && req.status === 200) {
-            let res = JSON.parse(req.responseText)
-            //want results to be random
-            let i = getRandomInt()
-            let videoId = res["items"][i]["id"]["videoId"]
-            let url = videoUrl + videoId
-            console.log(url)
-            return url
-        } else {
-            console.log("request got status: " + req.status + "error: " + req.statusText)
-            return defaultUrl
-        }
-    }
+const getUrl = () => {
+	let i = getRandomInt()
+	return baseUrl + videoIds[i]
 }
 
+// Look for requests to Facebook, redirect to a cat video
 chrome.webRequest.onBeforeRequest.addListener(
     (details) => {
-        return {redirectUrl: defaultUrl}
+		let url = getUrl()
+        return {redirectUrl: url}
     },
     {urls: ["*://www.facebook.com/*", "*://facebook.com/*"]},
     ["blocking"]
 )
-
